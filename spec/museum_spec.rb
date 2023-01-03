b3734ed104111cca71d7d0efbfce893f054c3116
Jade Stewart <jadekstewart3@gmail.com>
2023-01-03 10:24:09 -0700

    Create recommend_exhibits method with passing test

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -5,9 +5,13 @@ require './lib/exhibit'
 RSpec.describe Museum do
 
   let(:dmns) { Museum.new("Denver Museum of Nature and Science") }
+
   let(:gems_and_minerals) { Exhibit.new({name: "Gems and Minerals", cost: 0}) }
   let(:dead_sea_scrolls) { Exhibit.new({name: "Dead Sea Scrolls", cost: 10}) }
   let(:imax) { Exhibit.new({name: "IMAX",cost: 15}) }
+
+  let (:patron_1) { Patron.new("Bob", 20) }
+  let (:patron_2) { Patron.new("Sally", 20) }
 
   describe "#initialize" do 
     it "exists" do 
@@ -27,6 +31,23 @@ RSpec.describe Museum do
       dmns.add_exhibit(imax)
 
       expect(dmns.exhibits).to eq([gems_and_minerals, dead_sea_scrolls, imax])
+    end
+  end
+
+  describe "#recommend_exhibits" do 
+    it "returns array of exhibits to recommend to patrons" do 
+      patron_1.add_interest("Dead Sea Scrolls")
+      patron_1.add_interest("Gems and Minerals")
+
+      patron_2.add_interest("IMAX")
+
+      dmns.add_exhibit(dead_sea_scrolls)
+      dmns.add_exhibit(gems_and_minerals)
+      dmns.add_exhibit(imax)
+
+      expect(dmns.recommend_exhibits(patron_1)).to eq([dead_sea_scrolls, gems_and_minerals])
+      expect(dmns.recommend_exhibits(patron_2)).to eq([imax])
+
     end
   end
 end
