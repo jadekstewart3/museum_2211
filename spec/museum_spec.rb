dd96138309f003d37cafa29c444b54c6f7672dc9
Jade Stewart <jadekstewart3@gmail.com>
2023-01-03 09:26:51 -0700

    Create Museum class with passing initialization test

---
+++ spec/museum_spec.rb
@@ -0,0 +1,19 @@
+require './lib/museum'
+require './lib/patron'
+require './lib/exhibit'
+
+RSpec.describe Museum do
+
+  let(:dmns) { Museum.new("Denver Museum of Nature and Science") }
+
+  describe "#initialize" do 
+    it "exists" do 
+      expect(dmns).to be_an_instance_of(Museum)
+    end
+    
+    it "has attributes" do 
+      expect(dmns.name).to eq("Denver Museum of Nature and Science")
+      expect(dmns.exhibits).to eq([])
+    end
+  end
+end
