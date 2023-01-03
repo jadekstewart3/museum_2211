e8d395b43a4cd4b1fe44526969649f4327cce705
Jade Stewart <jadekstewart3@gmail.com>
2023-01-03 10:34:17 -0700

    Create admit method with passing test

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -12,6 +12,7 @@ RSpec.describe Museum do
 
   let (:patron_1) { Patron.new("Bob", 20) }
   let (:patron_2) { Patron.new("Sally", 20) }
+  let (:patron_3) { Patron.new("Johnny", 5) }
 
   describe "#initialize" do 
     it "exists" do 
@@ -48,6 +49,29 @@ RSpec.describe Museum do
       expect(dmns.recommend_exhibits(patron_1)).to eq([dead_sea_scrolls, gems_and_minerals])
       expect(dmns.recommend_exhibits(patron_2)).to eq([imax])
 
+    end
+  end
+
+  describe "#admit" do 
+    before do 
+      dmns.add_exhibit(dead_sea_scrolls)
+      dmns.add_exhibit(gems_and_minerals)
+      dmns.add_exhibit(imax)
+
+      patron_1.add_interest("Dead Sea Scrolls")
+      patron_1.add_interest("Gems and Minerals")
+
+      patron_2.add_interest("Dead Sea Scrolls")
+
+      patron_3.add_interest("Dead Sea Scrolls")
+
+      dmns.admit(patron_1)
+      dmns.admit(patron_2)
+      dmns.admit(patron_3)
+    end
+
+    it "admits patrons" do 
+      expect(dmns.patrons).to eq([patron_1, patron_2, patron_3])
     end
   end
 end
