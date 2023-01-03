139e3018407c101e92d9965b4d27d7a623cf4220
Jade Stewart <jadekstewart3@gmail.com>
2023-01-03 09:20:05 -0700

    Create add_interest method with passing test

--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe Patron do
 
   describe "#initialize" do 
 
-    it " exists" do 
+    it "exists" do 
       expect(patron_1).to be_an_instance_of(Patron)
     end
 
@@ -15,6 +15,15 @@ RSpec.describe Patron do
       expect(patron_1.name).to eq("Bob")
       expect(patron_1.spending_money).to eq(20)
       expect(patron_1.interests).to eq([])
+    end
+  end
+
+  describe "#add_interest" do 
+    it "adds exhibit names to interests array" do 
+      patron_1.add_interest("Dead Sea Scrolls")
+      patron_1.add_interest("Gems and Minerals")
+
+      expect(patron_1.interests).to eq(["Dead Sea Scrolls", "Gems and Minerals"])
     end
   end
 end
